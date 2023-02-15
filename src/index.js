e65bf988031f45360b095921a814911055227e62
ellentrj <125470257+ellentrj@users.noreply.github.com>
2023-02-15 10:38:21 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+
+let numero = 1
+{
+  let numero = 2
+  console.log('dentro = ', numero)
+}
+console.log('fora =' , numero)
+
+//declarando variavel com var
+
+var numero1 = 1
+{
+  var numero1 = 2
+  console.log('dentro1 =' , numero1)
+
+}
+console.log('fora1 = ', numero1)
+
+//declarano uma constante
+
+const numero2 = 10
+numero2 = 11
+console.log(numero2)
